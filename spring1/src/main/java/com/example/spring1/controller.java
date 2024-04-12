678c4404fad48f4b88c9ada837180a85440613f7
Aswin <117593557+AswinM1@users.noreply.github.com>
2024-04-12 12:37:43 +0530

    Add files via upload

---
+++ spring1/src/main/java/com/example/spring1/controller.java
@@ -0,0 +1,13 @@
+package com.example.spring1;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @GetMapping("/aswin")
+    public String get(aq22)
+    {
+        return "Welcome";
+    }
+        
+}
